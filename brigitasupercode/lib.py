ec3dd6370adcf29fda5b8a312ae11552958dd597
brigita polgariova <brigita@pobtrading.com>
2021-11-27 16:00:07 +0800

    added re

--- brigitasupercode/lib.py
+++ brigitasupercode/lib.py
@@ -1,12 +1,12 @@
 
 import cryptocode
-import random as rand
+import random 
 
 
 def try_me():
     print("Welcome in decoding program")
     batch_708 = ['Lisa', 'Brigita', 'Regina', 'Mike', 'Claire', 'Ben1', 'Ben2']
-    name = rand.choice(batch_708)
+    name = random.choice(batch_708)
     encoded_name = cryptocode.encrypt(name, "cryptocode is amazing")
     return f" This is your classmate encoded name: {encoded_name}"
     
